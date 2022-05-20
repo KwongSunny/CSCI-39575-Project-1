d97a982a8223c1cf64f72356c18c46ee86bcbcaf
KwongSunny <sunnykwong91500@gmail.com>
2022-05-19 20:46:25 -0400

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,73 @@
+// const myHeading = document.querySelector('h1');
+// myHeading.textContent = 'Hello world!';
+
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === '../images/firefox-icon-(Small).png') {
+    myImage.setAttribute ('src','../images/cattail.jfif');
+  } else {
+    myImage.setAttribute ('src','../images/firefox-icon-(Small).png');
+  }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'I like Pi, ' + myName + '!';
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'I like Pi, ' + storedName + '!';
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+
+/*
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+
+// function setUserName() {
+//     let myName = prompt('Please enter your name.');
+//     localStorage.setItem('name', myName);
+//     myHeading.textContent = 'Mozilla is cool, ' + myName;
+//   }
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.innerHTML = 'I like Pi, ' + myName + '!';
+    }
+  }
+  
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+  }
+  
+myButton.onclick = function() {
+    setUserName();
+  }
+
+*/
